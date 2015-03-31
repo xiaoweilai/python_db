b380afd3d2f9b9f1f036b35454149308d5f76113
yjduoduo <wxjlmr@126.com>
2015-03-31 21:40:33 +0800

    修改Frame

--- gui/Frame.py
+++ gui/Frame.py
@@ -6,11 +6,18 @@
 from Tkinter import *
 
 root = Tk()
+
+topframe = Frame(root)
+topframe.pack( side = TOP )
+
 frame = Frame(root)
 frame.pack()
 
 bottomframe = Frame(root)
 bottomframe.pack( side = BOTTOM )
+
+
+
 
 redbutton = Button(frame, text="Red", fg="red")
 redbutton.pack( side = LEFT)
@@ -23,5 +30,10 @@ bluebutton.pack( side = LEFT )
 
 blackbutton = Button(bottomframe, text="Black", fg="black")
 blackbutton.pack( side = BOTTOM)
+
+
+topbutton = Button(topframe, text="Yellow", fg="yellow")
+topbutton.pack( side = TOP)
+
 
 root.mainloop()
