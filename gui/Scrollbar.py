b99f40d0270ffbea17edbff44d391ff45df08f6b
yjduoduo <wxjlmr@126.com>
2015-03-31 22:24:17 +0800

    添加Text及颜色更改标签

--- gui/Scrollbar.py
+++ gui/Scrollbar.py
@@ -9,7 +9,7 @@ scrollbar = Scrollbar(root)
 scrollbar.pack( side = RIGHT, fill=Y )
 
 mylist = Listbox(root, yscrollcommand = scrollbar.set )
-for line in range(100):
+for line in range(1000):
    mylist.insert(END, "This is line number " + str(line))
 
 mylist.pack( side = LEFT, fill = BOTH )
