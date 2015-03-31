0bad72cb427211bf24d226d470936c245a4318c5
yjduoduo <wxjlmr@126.com>
2015-03-31 22:03:04 +0800

    修改MenuButton 一个菜单

--- gui/Menubutton.py
+++ gui/Menubutton.py
@@ -15,11 +15,15 @@ mb["menu"]  =  mb.menu
     
 mayoVar  = IntVar()
 ketchVar = IntVar()
+addVar   = IntVar()
 
 mb.menu.add_checkbutton ( label="mayo",
                           variable=mayoVar )
 mb.menu.add_checkbutton ( label="ketchup",
                           variable=ketchVar )
+mb.menu.add_checkbutton ( label="add",
+						  variable=addVar)						  
+
 
 mb.pack()
 top.mainloop()
