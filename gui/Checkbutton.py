8211c651514fce0c315db65bc51ebf029d963a39
yjduoduo <wxjlmr@126.com>
2015-03-31 21:19:23 +0800

    checkbutton小部件用于显示切换按钮的用户多项选择

---
+++ gui/Checkbutton.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+#coding=utf-8
+
+from Tkinter import *
+import tkMessageBox
+import Tkinter
+
+top = Tkinter.Tk()
+CheckVar1 = IntVar()
+CheckVar2 = IntVar()
+C1 = Checkbutton(top, text = "Music", variable = CheckVar1, \
+                 onvalue = 1, offvalue = 0, height=5, \
+                 width = 20)
+C2 = Checkbutton(top, text = "Video", variable = CheckVar2, \
+                 onvalue = 1, offvalue = 0, height=5, \
+                 width = 20)
+C1.pack()
+C2.pack()
+top.mainloop()
