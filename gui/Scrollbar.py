3f8e98b43de8ef1f0471483e65331859c0234ec2
yjduoduo <wxjlmr@126.com>
2015-03-31 22:19:26 +0800

    Scrollbar 滚动条

---
+++ gui/Scrollbar.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+#coding=utf-8
+
+from Tkinter import *
+
+root = Tk()
+scrollbar = Scrollbar(root)
+scrollbar.pack( side = RIGHT, fill=Y )
+
+mylist = Listbox(root, yscrollcommand = scrollbar.set )
+for line in range(100):
+   mylist.insert(END, "This is line number " + str(line))
+
+mylist.pack( side = LEFT, fill = BOTH )
+scrollbar.config( command = mylist.yview )
+
+mainloop()
