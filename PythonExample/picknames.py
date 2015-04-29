9e340369494d26c7ae46615306e05465419125d8
yjduoduo <wxjlmr@126.com>
2015-04-30 00:23:49 +0800

    python必须用tab键才能识别，这和其他的语言格式有点冲突

--- PythonExample/picknames.py
+++ PythonExample/picknames.py
@@ -8,8 +8,7 @@ import os
 filenames=os.listdir(os.getcwd())
 for name in filenames:
 	print name
-	print name[:-3]
-    filenames[filenames.index(name)]=name[:-3]
+	filenames[filenames.index(name)]=name[:-3]
 out=open('names.txt','w')
 for name in filenames:
      out.write(name+'\n')
