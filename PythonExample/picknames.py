b311f275c5cf3dbda1cb1f7c7caee247c490f0da
yjduoduo <wxjlmr@126.com>
2015-04-29 23:09:30 +0800

    添加PythonExample 示例集合 提取文件名 - 类似a.rm 无法提取类似于.txt

---
+++ PythonExample/picknames.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+#coding=utf-8
+
+
+# --- picknames.py ---
+import os
+filenames=os.listdir(os.getcwd())
+for name in filenames:
+	print name
+	print name[:-3]
+    filenames[filenames.index(name)]=name[:-3]
+out=open('names.txt','w')
+for name in filenames:
+     out.write(name+'\n')
+out.close()
+
